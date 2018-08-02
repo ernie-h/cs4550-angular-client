a4f5fa0bbb9db3e7ce5896df4b5fae11aa3992f6
ernie-hao <pdphiro@gmail.com>
2018-08-02 09:55:18 -0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WhiteBoardComponent } from './white-board/white-board.component';
+import {CourseServiceClient} from './services/course.service.client'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WhiteBoardComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    CourseServiceClient
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/white-board/white-board.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseServiceClient} from '../services/course.service.client'
+
+@Component({
+  selector: 'app-white-board',
+  templateUrl: './white-board.component.html',
+  styleUrls: ['./white-board.component.css']
+})
+export class WhiteBoardComponent implements OnInit {
+  courses: [];
+  constructor(private courseService: CourseServiceClient) { }
+
+  ngOnInit() {
+    this.courseService.findAllCourses()
+    .then((courses) => this.courses = courses);
+  }
+
+}
