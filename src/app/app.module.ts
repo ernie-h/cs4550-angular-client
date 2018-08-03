7dcd000003108305e0ae65e1c76c125d58a81ded
ernie-hao <pdphiro@gmail.com>
2018-08-03 13:15:06 -0400

    Render course (etc) lists in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StyledComponentsModule } from 'angular-styled-components';
 
 import { AppComponent } from './app.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
-import {CourseServiceClient} from './services/course.service.client'
+import {CourseServiceClient} from './services/course.service.client';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WhiteBoardComponent
+    WhiteBoardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StyledComponentsModule,
   ],
   providers: [
     CourseServiceClient
