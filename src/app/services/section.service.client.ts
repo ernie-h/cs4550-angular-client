f794e6667df3f3432bb9d25e46f9b48724fb3792
ernie-hao <pdphiro@gmail.com>
2018-08-06 14:30:29 -0400

    Styling for home/delete section

--- src/app/services/section.service.client.ts
+++ src/app/services/section.service.client.ts
@@ -1,14 +1,14 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 const COURSE_API_URL = "http://localhost:3000/api/course";
-const COURSE_ID_API_URL = "http://localhost:3000/api/course/CID";
+const SECTION_ID_API_URL = "http://localhost:3000/api/section/SID";
 const COURSE_ID_SECTION_API_URL = "http://localhost:3000/api/course/CID/section";
 @Injectable()
 export class SectionServiceClient {
 
   enroll = sectionId =>
     fetch('http://localhost:3000/api/section/' + sectionId + '/enroll', {
-      method: 'put',
+      method: 'PUT',
       credentials: 'include'
     })
 
@@ -22,7 +22,7 @@ export class SectionServiceClient {
 
   createSection = section =>
     fetch('http://localhost:3000/api/section', {
-      method: 'post',
+      method: 'POST',
       headers: {
         'content-type': 'application/json'
       },
@@ -30,4 +30,11 @@ export class SectionServiceClient {
       body: JSON.stringify(section)
     })
       .then(response => response.json())
+
+  deleteSection = (sectionId) =>
+    fetch(SECTION_ID_API_URL.replace('SID', sectionId), {
+      method: 'delete'
+    })
+    .then(response => response.status)
+
 }
